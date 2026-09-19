a750a94424309b283eaf2a1f36604a3ef1e27c55
Helen Garfinkle <helgarfinkle@gmail.com>
2026-09-19 14:15:01 -0700

    simple grid

---
+++ src/routes/Grid.svelte
@@ -0,0 +1,12 @@
+<script>
+    const inset = 10
+</script>
+
+<svg width=600 height=600>
+    <rect width=500 height=500 x={inset} y={inset} style="fill:white;stroke-width:4;stroke:black"/>
+    {#each {length: 10}, row }
+        {#each {length: 10}, column }
+            <rect width=50 height=50 x={row * 50 + inset} y={column * 50 + inset} style="fill:white;stroke-width:1;stroke:black" />
+        {/each}
+    {/each}
+</svg>
