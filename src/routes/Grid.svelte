09943387875436a580dcd540177b5f1a6a72e925
Helen Garfinkle <helgarfinkle@gmail.com>
2026-09-19 14:34:01 -0700

    format

--- src/routes/Grid.svelte
+++ src/routes/Grid.svelte
@@ -1,16 +1,28 @@
 <script>
-    const inset = 10
-    const squareSize = 50
-    const gridSize = 10
+	const inset = 10;
+	const squareSize = 50;
+	const gridSize = 10;
 </script>
 
 <svg width={squareSize * gridSize + 2 * inset} height={squareSize * gridSize + 2 * inset}>
-    <svg x={inset} y={inset}>   
-    <rect width={squareSize * gridSize} height={squareSize * gridSize} x={inset} y={inset} style="fill:white;stroke-width:4;stroke:black" />  
-        {#each {length: gridSize}, row }
-            {#each {length: gridSize}, column }
-                <rect width={squareSize} height={squareSize} x={row * squareSize} y={column * squareSize} style="fill:white;stroke-width:1;stroke:black" />
-            {/each}
-        {/each}
-    </svg>
+<svg x={inset} y={inset}>
+		<rect
+			width={squareSize * gridSize}
+			height={squareSize * gridSize}
+			x={inset}
+			y={inset}
+			style="fill:white;stroke-width:4;stroke:black"
+		/>
+		{#each { length: gridSize }, row}
+			{#each { length: gridSize }, column}
+				<rect
+					width={squareSize}
+					height={squareSize}
+					x={row * squareSize}
+					y={column * squareSize}
+					style="fill:white;stroke-width:1;stroke:black"
+				/>
+			{/each}
+		{/each}
+	</svg>
 </svg>
