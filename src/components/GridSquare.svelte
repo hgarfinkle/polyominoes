73b61f04a0ec0e44826cbcf30dcd95a6e40051b7
Helen Garfinkle <helgarfinkle@gmail.com>
2026-09-20 19:13:28 -0700

    animating cell clicking

--- src/components/GridSquare.svelte
+++ src/components/GridSquare.svelte
@@ -1,5 +1,12 @@
 <script lang="ts">
+	import { Tween } from 'svelte/motion';
+
 	let { squareSize, row, column, filled, onClick } = $props();
+
+	const darkness = Tween.of<number>(() => (filled ? 0 : 1), { duration: 50 });
+	function handleClick() {
+		onClick();
+	}
 </script>
 
 <rect
@@ -7,8 +14,9 @@
 	height={squareSize}
 	x={row * squareSize}
 	y={column * squareSize}
-	class={['cell', filled ? 'filled' : 'unfilled']}
-	onclick={onClick}
+	class="cell"
+	onclick={handleClick}
+	style={`fill: oklch(${darkness.current} 0 0)`}
 />
 
 <style>
@@ -16,13 +24,5 @@
 		stroke-width: 1;
 		stroke: black;
 		cursor: pointer;
-
-		&.filled {
-			fill: black;
-		}
-
-		&.unfilled {
-			fill: white;
-		}
 	}
 </style>
